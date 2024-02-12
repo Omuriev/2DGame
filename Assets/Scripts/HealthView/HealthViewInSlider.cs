88e1fbefbf9d3cb3441f487af33622cb97c08255
Omuriev <oleguer132@gmail.com>
2024-02-12 20:57:02 +0300

    changed HealthViewInSlider script

--- Assets/Scripts/HealthView/HealthViewInSlider.cs
+++ Assets/Scripts/HealthView/HealthViewInSlider.cs
@@ -3,11 +3,6 @@ using UnityEngine.UI;
 
 public class HealthViewInSlider : HealthSliderView
 {
-    private void OnEnable()
-    {
-        Health.ChangedHealth += ChangeHealth;
-    }
-
     public override void ChangeHealth(float currentHealth, float maxHealth)
     {
         Slider.value = (currentHealth / maxHealth);
